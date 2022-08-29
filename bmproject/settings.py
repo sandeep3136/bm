1b64e43e3926b7a1ef6e82cf56c97e6629b5701c
Sandeep Angara <sandeep333jab@gmail.com>
2022-08-29 13:03:09 +0530

    changes

--- bmproject/settings.py
+++ bmproject/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-    
+STATICFILES_DIRS = ( os.path.join('static'), )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
