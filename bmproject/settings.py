b366d33d18fa5836703c3b3a7a2bd95c9b614053
Sandeep Angara <sandeep333jab@gmail.com>
2022-09-13 14:29:49 +0530

    setup postgres

--- bmproject/settings.py
+++ bmproject/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'bmproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'angara',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
